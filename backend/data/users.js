239b474464159e91f70193f5df81abcf7dc7d17d
rextivlan <affan.ali368@gmail.com>
2022-02-10 23:01:24 +0530

    Add Phone Number to User Model in backend

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,23 +4,27 @@ const users = [
   {
     name: "Admin User",
     email: "admin@mart.com",
+    phone: "7993918786",
     password: bcrypt.hashSync("1234567", 10),
     isAdmin: true,
   },
   {
     name: "Ali Affan",
     email: "ali@mart.com",
+    phone: "7993918523",
     password: bcrypt.hashSync("1234567", 10),
     isAdmin: true,
   },
   {
     name: "Lone Walker",
     email: "walker@mart.com",
+    phone: "1234567809",
     password: bcrypt.hashSync("1234567", 10),
   },
   {
     name: "Tayeeb Hasan",
     email: "tayeeb@mart.com",
+    phone: "7702647910",
     password: bcrypt.hashSync("1234567", 10),
     isAdmin: true,
   },
